ca121b1db557ab24f3a3d7dc2860c3ff76259cd2
firmao <minhamae123>
2015-03-14 07:52:35 +0100

    new

---
+++ GazeTrackerUI/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace GazeTrackerUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblPupilLeft = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPupilRight = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(121, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(121, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 21);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblPupilLeft
+            // 
+            this.lblPupilLeft.AutoSize = true;
+            this.lblPupilLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPupilLeft.Location = new System.Drawing.Point(48, 31);
+            this.lblPupilLeft.Name = "lblPupilLeft";
+            this.lblPupilLeft.Size = new System.Drawing.Size(19, 20);
+            this.lblPupilLeft.TabIndex = 2;
+            this.lblPupilLeft.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Left pupil diameter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Right pupil diameter";
+            // 
+            // lblPupilRight
+            // 
+            this.lblPupilRight.AutoSize = true;
+            this.lblPupilRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPupilRight.Location = new System.Drawing.Point(217, 31);
+            this.lblPupilRight.Name = "lblPupilRight";
+            this.lblPupilRight.Size = new System.Drawing.Size(19, 20);
+            this.lblPupilRight.TabIndex = 5;
+            this.lblPupilRight.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 249);
+            this.Controls.Add(this.lblPupilRight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPupilLeft);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Adptive Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblPupilLeft;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPupilRight;
+    }
+}
---
+++ GazeTrackerUI/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace GazeTrackerUI
+{
+    public partial class Form1 : Form
+    {
+        private Thread currentTimeThread = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            currentTimeThread = new Thread(new ThreadStart(CountTime));
+            currentTimeThread.IsBackground = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void CountTime()
+        {
+            while (true)
+            {
+                Invoke(new UpdateTimeDelegate(updateCurrentTime));
+                Thread.Sleep(1000);
+            }
+        }
+
+
+        private void updateCurrentTime()
+        {
+            try
+            {
+                double dL = GazeTrackingLibrary.Tracker.dL;
+                double dR = GazeTrackingLibrary.Tracker.dR;
+                if (dL > 0 && dR > 0)
+                {
+                    lblPupilLeft.Text = "" + dL;
+                    lblPupilRight.Text = "" + dR;
+                    button1.SetBounds(button1.Location.X, button1.Location.Y
+                    , Convert.ToInt32(dL), Convert.ToInt32(dR) );
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Error: " + ex.StackTrace);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentTimeThread.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+			currentTimeThread.Abort();
+			currentTimeThread.Join();
+
+			currentTimeThread = new Thread(new ThreadStart(CountTime));
+			currentTimeThread.IsBackground = true;
+        }
+    }
+    public delegate void UpdateTimeDelegate();
+}
